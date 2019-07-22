d2f6626a8889b87cc3ef4d894f26672192573151
Stacy Johns <stacykjohns@icloud.com>
2019-07-22 23:16:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,17 +1,19 @@
-def roll_call_dwarves(array)# code an argument here
+def roll_call_dwarves(array)
   array.each_with_index do |value, index|
     puts "#{index + 1}. #{value} "
   end
 end
 
-def summon_captain_planet(array)# code an argument here
+def summon_captain_planet(array)
   array.map do |value|
      value.capitalize + "!"
   end
 end
 
 def long_planeteer_calls(array)
-  array.any?
+  array.any? do |value|
+    value.length <= 4
+  end
 end
 
 def find_the_cheese(array)
