b6852d667c01bb249a8f2ca1425651597de420f2
Stacy Johns <stacykjohns@icloud.com>
2019-07-22 23:17:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,9 +11,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any? do |word|
-    word.length <= 4
-  end
+  array.any? { |word| word.length <= 4}
 end
 
 def find_the_cheese(array)
