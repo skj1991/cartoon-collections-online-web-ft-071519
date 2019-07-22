06f6dc900899a95666c38d7e5812b5bc69939e6c
Stacy Johns <stacykjohns@icloud.com>
2019-07-22 22:50:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,9 +5,7 @@ def roll_call_dwarves(array)# code an argument here
 end
 
 def summon_captain_planet(array)# code an argument here
-  array.map do |value|
-    "#{value}!"
-    value = value.first.upcase
+  array.map(&:capitalize).join(' ')
   end
 end
 
